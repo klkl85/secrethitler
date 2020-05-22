c1a74412892da4bd2617e9d00b2e941106a3e2ee
klkl85 <g1tHu3@klkl.co.uk>
2020-05-22 01:20:03 +0100

    Different Blame TypeError: choice() takes 2 positional arguments problem persisted previously

--- secret_hitler.py
+++ secret_hitler.py
@@ -1175,7 +1175,9 @@ class Game(object):
                 chancy_tag = self.chancellor.get_markdown_tag()
 
             if self.game_state == GameStates.ELECTION:
-                return "People who haven't yet voted: {}\n".format(random.choice(['👇🏻', '👇🏼', '👇🏽', '👇🏾', '👇🏿'])) + self.list_nonvoters()
+                hands = ["👇🏻", "👇🏼", "👇🏽", "👇🏾", "👇🏿", "👇"]
+                random.shuffle(hands)
+                return "People who haven't yet voted: {}\n{}".format(hands[0], self.list_nonvoters())
             elif self.game_state == GameStates.CHANCY_NOMINATION:
                 return "{} 🗳 needs to nominate a chancellor!".format(pres_tag)
             elif self.game_state == GameStates.LEG_PRES:
